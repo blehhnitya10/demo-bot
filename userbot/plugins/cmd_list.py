4e1201b07116826a3734c22e4a264ccafcc88a95
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2021-05-23 20:04:32 +0530

    Update cmd_list.py

--- userbot/plugins/cmd_list.py
+++ userbot/plugins/cmd_list.py
@@ -5,11 +5,11 @@ from REBELBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot import bot as REBELBOT
 from userbot import ALIVE_NAME
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "rebel User"
-rebel_logo = "./REBEL725/REBELBOT_logo.jpg"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "REBEL User"
+REBEL_logo = "./REBEL725/REBELBOT_logo.jpg"
 
-@rebelbot.on(admin_cmd(pattern=r"cmds"))
-@rebelbot.on(sudo_cmd(pattern=r"cmds", allow_sudo=True))
+@REBELBOT.on(admin_cmd(pattern=r"cmds"))
+@REBELBOT.on(sudo_cmd(pattern=r"cmds", allow_sudo=True))
 async def install(event):
     if event.fwd_from:
         return
@@ -17,7 +17,7 @@ async def install(event):
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
     cmd = "ls userbot/plugins"
-    thumb = rebel_logo
+    thumb = REBEL_logo
     process = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
@@ -25,11 +25,11 @@ async def install(event):
     o = stdout.decode()
     _o = o.split("\n")
     o = "\n".join(_o)
-    OUTPUT = f"List of Plugins in rebelbot :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/rebelbot_Chit_Chat for help."
+    OUTPUT = f"List of Plugins in REBELBOT :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/REBELBOT_Chit_Chat for help."
     if len(OUTPUT) > 69:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
             out_file.name = "cmd_list.text"
-            rebel_file = await bot.send_file(
+            REBEL_file = await bot.send_file(
                 event.chat_id,
                 out_file,
                 force_document=True,
@@ -37,5 +37,5 @@ async def install(event):
                 thumb=thumb,
                 reply_to=reply_to_id,
             )
-            await edit_or_reply(rebel_file, f"Output Too Large. This is the file for the list of plugins in rebelbot.\n\n**BY :-** {DEFAULTUSER}")
+            await edit_or_reply(REBEL_file, f"Output Too Large. This is the file for the list of plugins in REBELBOT.\n\n**BY :-** {DEFAULTUSER}")
             await event.delete()
