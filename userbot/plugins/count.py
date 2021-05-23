ae5ecaf51bb61717dd6b954501ccf710e3be4847
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2021-05-23 23:53:08 +0530

    Update count.py

--- userbot/plugins/count.py
+++ userbot/plugins/count.py
@@ -65,24 +65,24 @@ async def stats(
     stop_time = time.time() - start_time
     full_name = inline_mention(await event.client.get_me())
     response = f"🔰**Stats for {full_name}**🔰\n\n"
-    response += f"🔱 **Private Chats:** {private_chats} \n"
+    response += f"😜 **Private Chats:** {private_chats} \n"
     response += f"🔸   `Users: {private_chats - bots}` \n"
     response += f"🔹   `Bots: {bots}` \n"
-    response += f"🔱 **Groups:** {groups} \n"
-    response += f"🔱 **Channels:** {broadcast_channels} \n"
-    response += f"☣️  **Admin in Groups:** {admin_in_groups} \n"
-    response += f"🔹   `Creator: {creator_in_groups}` \n"
-    response += f"🔸   `Admin Rights: {admin_in_groups - creator_in_groups}` \n"
-    response += f"☣️  **Admin in Channels:** {admin_in_broadcast_channels} \n"
+    response += f"😌 **Groups:** {groups} \n"
+    response += f"😌 **Channels:** {broadcast_channels} \n"
+    response += f"‼️  **Admin in Groups:** {admin_in_groups} \n"
+    response += f"🤣   `Creator: {creator_in_groups}` \n"
+    response += f"❌   `Admin Rights: {admin_in_groups - creator_in_groups}` \n"
+    response += f"❌  **Admin in Channels:** {admin_in_broadcast_channels} \n"
     response += f"🔸   `Creator: {creator_in_channels}` \n"
     response += (
         f"🔹   `Admin Rights: {admin_in_broadcast_channels - creator_in_channels}` \n"
     )
-    response += f"🔱 **Unread:** {unread} \n"
-    response += f"🔱 **Unread Mentions:** {unread_mentions} \n\n"
-    response += f"☣️   __It Took:__ {stop_time:.02f}s \n"
+    response += f"🔥 **Unread:** {unread} \n"
+    response += f"🤘 **Unread Mentions:** {unread_mentions} \n\n"
+    response += f"⚜️   __It Took:__ {stop_time:.02f}s \n"
     response += (
-        f"📌 **From The DataBase Of** :- [REBELBOT](https://t.me/REBELBOT_Support)"
+        f"📌 **From The DataBase Of** :- [REBELBOT](https://t.me/REBELBOT_SUPPORT)"
     )
     await hell.edit(response)
 
