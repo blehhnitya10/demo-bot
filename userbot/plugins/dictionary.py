80163e8e585dc6fdfdda656f47b817846ee85220
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2021-05-24 21:38:56 +0530

    Update dictionary.py

--- userbot/plugins/dictionary.py
+++ userbot/plugins/dictionary.py
@@ -36,10 +36,10 @@ async def _(event):
         return
     word = event.pattern_match.group(1)
     dictionary = PyDictionary()
-    rebel = dictionary.meaning(word)
+    REBEL = dictionary.meaning(word)
     output = f"**Word :** __{word}__\n\n"
     try:
-        for a, b in rebel.items():
+        for a, b in REBEL.items():
             output += f"**{a}**\n"
             for i in b:
                 output += f"☞__{i}__\n"
