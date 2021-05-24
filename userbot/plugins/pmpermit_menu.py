fbd193570dd83438a2d24f1ecec74185841e085a
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2021-05-24 23:36:01 +0530

    Update pmpermit_menu.py

--- userbot/plugins/pmpermit_menu.py
+++ userbot/plugins/pmpermit_menu.py
@@ -11,7 +11,7 @@ from telethon import functions
 from userbot.plugins.sql_helper import pmpermit_sql as pmpermit_sql
 from userbot import ALIVE_NAME
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "rebel User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "REBEL User"
 PREV_REPLY_MESSAGE = {}
 
 
