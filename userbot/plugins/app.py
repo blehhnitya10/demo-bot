879e42f7b163acf625471d399fab993e7b83137e
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2021-05-24 17:07:35 +0530

    Update app.py

--- userbot/plugins/app.py
+++ userbot/plugins/app.py
@@ -10,7 +10,7 @@ from userbot import ALIVE_NAME, CMD_HELP
 from REBELBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "rebel User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "REBEL User"
 
 
 @bot.on(admin_cmd(pattern="app (.*)"))
