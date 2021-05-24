76974bca0faad79e096305fc6d82303e1d16ea2a
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2021-05-25 02:24:53 +0530

    Update calladmin.py

--- userbot/plugins/calladmin.py
+++ userbot/plugins/calladmin.py
@@ -2,7 +2,7 @@
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
-from REBELBOT.util import admin_cmd
+from REBELBOT.utils import admin_cmd
 
 
 @borg.on(admin_cmd(pattern="admins"))
