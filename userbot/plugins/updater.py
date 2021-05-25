7fce6cc0e17247e0196c4318bf501b72a00b00a6
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2021-05-25 20:50:06 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -15,13 +15,13 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "please check out to an official branch, and re-start the updater."
 )
 OFFICIAL_UPSTREAM_REPO = Config.UPSTREAM_REPO
-BOT_IS_UP_TO_DATE = "**The 𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋** is up-to-date sur."
+BOT_IS_UP_TO_DATE = "**The 𝗥𝗘𝗕𝗘𝗟𝗕𝗢𝗧** is up-to-date sur."
 NEW_BOT_UP_DATE_FOUND = (
     "new update found for {branch_name}\n"
     "changelog: \n\n{changelog}\n"
-    "updating your 𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋 ..."
+    "updating your 𝗥𝗘𝗕𝗘𝗟𝗕𝗢𝗧 ..."
 )
-NEW_UP_DATE_FOUND = "New update found for {branch_name}\n" "`updating your 𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋...`"
+NEW_UP_DATE_FOUND = "New update found for {branch_name}\n" "`updating your 𝗥𝗘𝗕𝗘𝗟𝗕𝗢𝗧...`"
 REPO_REMOTE_NAME = "temponame"
 IFFUCI_ACTIVE_BRANCH_NAME = "master"
 DIFF_MARKER = "HEAD..{remote_name}/{branch_name}"
