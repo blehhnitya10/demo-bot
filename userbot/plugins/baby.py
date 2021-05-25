f7459768d4d0f632828cdbc0baa7a93fd72a52e0
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2021-05-26 00:55:40 +0530

    Update baby.py

--- userbot/plugins/baby.py
+++ userbot/plugins/baby.py
@@ -11,5 +11,4 @@ async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit("""HEYY BABY😍💏😘 HOW ARE  YOU  💏 AP KO PATA HA MA BASS AP KK LIYA HUU ISS DUNIYA MA ORR AP MERI JAAN HOO 💞💗😍🥰. ORR AP KKK BAAD KOI NAHI HAAA MERA 🥺🥺🥺. AP JAB NAHI HOO TII HO TABB KOI MERE SA BAAT  NAHI KARTA HAA 🥺🥺. BUT MERE KO ISS SA KOI FIRK NAHI PADTA HA😌😌. BASS AP MERA SATH HOO YAHI 🥰🥰. BHUT JADA HAA 💝❣️ I LOVE YOU MY SWEET HEART TUM MERI JAAN HO ORR MAA THUMARA. LIYA  KUCH VV KAR SAKTA HUUU """)
         
-CmdHelp("baby").add_command(
-  'baby'SEND YOUR GF😜😜'
+
