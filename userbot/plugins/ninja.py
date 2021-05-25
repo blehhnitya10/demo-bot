418e77973b376c7be8aa426119e77db4573cbd64
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2021-05-26 00:50:23 +0530

    Update ninja.py

--- userbot/plugins/ninja.py
+++ userbot/plugins/ninja.py
@@ -31,3 +31,5 @@ async def delete_it(safai):
                 await delme.client.send_message(
                     BOTLOG_CHATID, "Well, I can't delete a message"
                 )
+CmdHelp("del").add_command(
+  'del',
